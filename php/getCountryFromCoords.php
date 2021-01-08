698b530b6371ed5cc320f882a961d586f15f991a
Miriam Fernandes <miriam.fernandes.97@gmail.com>
2021-01-08 14:48:47 +0000

    changed the api from opencage to geonames.

--- php/getCountryFromCoords.php
+++ php/getCountryFromCoords.php
@@ -1,11 +1,15 @@
 <?php
-
+/*\this was what was done before
 $key = '46fdacac5d374155ba838d479dd73fab';
 
-$url = 'https://api.opencagedata.com/geocode/v1/json?q='.$_REQUEST['lat'].','.$_REQUEST['long'].'&pretty=1&key=' . $key;
-$ch = curl_init();
+$url = 'https://api.opencagedata.com/geocode/v1/json?q='.$_REQUEST['lat'].','.$_REQUEST['long'].'&pretty=1&key=' . $key; // change to the geonames api for getting country coordinates.
 
-//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+*/
+
+$url = 'http://api.geonames.org/countryCodeJSON?formatted=true&lat='. $_REQUEST['lat']. ',long='.$_REQUEST['lng'] . '&username=miriam97&style=full';
+
+	$ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // this line was commented out before.
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_URL,$url);
 
