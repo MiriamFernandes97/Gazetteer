0e885f8aff8d713e04442af62cd7c7b1e1767a96
Miriam Fernandes <miriam.fernandes.97@gmail.com>
2021-01-12 13:54:07 +0000

    .

--- php/getPolygon.php
+++ php/getPolygon.php
@@ -16,14 +16,15 @@ $output = $countries['features'];
 
 //this is for choosing which selector to use.
 
-if($_REQUEST['type'] == 'Feature'){ // it was 'code'
-    $selector = ['properties']['iso_a3']; 
-    $selector = ['properties']['name'];
+if($_REQUEST['type'] == 'code'){  // if the type ends up being in a code format, the selector will be the iso code else it will be by name.
+    $selector = 'iso_a3'; 
+}else{
+    $selector = 'name';
 
 }
 
 foreach($output as $key => $val ){ // this means the output should be in in key value pairs.
-    if($val['properties']['name'] == $_REQUEST['country']){ 
+    if($val['properties'][$selector] == $_REQUEST['country']){ 
         echo json_encode($val);
         break;
     }
